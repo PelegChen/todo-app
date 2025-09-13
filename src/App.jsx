a016df03561408f376c82a684d41be9d9c29a321
Chen Peleg <chenpeleg@gmail.com>
2025-09-13 14:36:48 +0300

    fasd

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([{
-    id : 1,
+    id : (Math.random() * 10000) ,
     text : 'Take the dog for a walk',
     isDone : false
   }])
-  const [count, setCount] = useState(0)
+ const addTodo = ( ) => {
+  const todoText = prompt('Add a todo')
+  const newTodo = {
+    text : todoText,
+    isDone : false,
+    id : (Math.random() * 10000)
+  }
+  setTodos([...todos, newTodo]) 
+ }
 
   return (
     <>
      
       <h1>My todo app</h1>
       <h2> Unfinished</h2>
-      <div>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
         {
          todos.map(todo=>  
            <div className="todo-item" key={todo.id}>
@@ -26,8 +34,10 @@ function App() {
         }
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
+        <button onClick={() => {
+          addTodo()
+        }}>
+         Add a todo
         </button>
       
       </div>
